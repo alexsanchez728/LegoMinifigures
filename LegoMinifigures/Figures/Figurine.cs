50e4dcd17bb54a052b84e2a3496d6820d71e22b3
Alex Sanchez <alesanchez728@gmail.com>
2018-01-15 17:49:05 -0600

    figure movements and 1 constructor

---
+++ LegoMinifigures/Figures/Figurine.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace LegoMinifigures.Figures
+{
+    class Figurine
+    {
+        //Hair/Hat, Head, Torso, Legs, Accessories, etc.
+        protected string _topper;
+        protected string _head;
+        protected string _toroso;
+        protected string _legs;
+        protected string _accessories;
+
+        public string Topper
+        {
+            get
+            {
+                return _topper;
+            }
+            set
+            {
+                _topper = value;
+            }
+        }
+        public string Head
+        {
+            get
+            {
+                return _head;
+            }
+            set
+            {
+                _head = value;
+            }
+        }
+        public string Toroso
+        {
+            get
+            {
+                return _toroso;
+            }
+            set
+            {
+                _toroso = value;
+            }
+        }
+        public string Legs
+        {
+            get
+            {
+                return _legs;
+            }
+            set
+            {
+                _legs = value;
+            }
+        }
+        public string Accessories
+        {
+            get
+            {
+                return _accessories;
+            }
+            set
+            {
+                _accessories = value;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Your Lego Figure:");
+            Console.WriteLine($"Topper: {_topper}");
+            Console.WriteLine($"Head: {_head}");
+            Console.WriteLine($"Toroso: {_toroso}");
+            Console.WriteLine($"Legs: {_legs}");
+            Console.WriteLine($"Accessories: {_accessories}");
+        }
+
+        public string MoveForward()
+        {
+            Console.WriteLine("Forward!");
+            return "forward";
+        }
+        public string MoveBackward()
+        {
+            Console.WriteLine("Back!");
+            return "back";
+        }
+        public string MoveLeft()
+        {
+            Console.WriteLine("Left!");
+            return "left";
+        }
+        public string MoveRight()
+        {
+            Console.WriteLine("Right!");
+            return "right";
+        }
+        public string Attack()
+        {
+            Console.WriteLine("Attack!");
+            return "attack";
+        }
+        public string Jump()
+        {
+            Console.WriteLine("Jump!");
+            return "jump";
+        }
+
+        public string Unlocks()
+        {
+            _accessories += ", Batmobile";
+            Display();
+            return "unlocked";
+        }
+
+
+        //            Methods
+        //If you were working at Tt Games, 
+        //            your minifigure classes might have actions associated with them in the form of:
+        //            ~> jump,
+        //            ~> double jump,
+        //            ~> attack,
+        //            ~> special attack,
+        //            ~> look,
+        //            ~> move methods.
+        //There might even be certain conditions that must be met to
+        //            construct (or unlock) a new character or other kind of object.
+    }
+}
--- LegoMinifigures/Program.cs
+++ LegoMinifigures/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LegoMinifigures.Figures;
+
 
 namespace LegoMinifigures
 {
@@ -10,6 +8,52 @@ namespace LegoMinifigures
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo cki;
+
+            Console.WriteLine("Press (ESC) to close");
+
+            var LegoGuy1 = new Figurine
+            {
+                Topper = "Red Cap",
+                Head = "Angry Eyes",
+                Toroso = "Adventurer",
+                Legs = "Brown",
+                Accessories = "Pickaxe"
+            };
+            LegoGuy1.Display();
+        do
+        {
+            cki = Console.ReadKey(true); // intercept = true; so keypress doesn't print
+                if (cki.Key == ConsoleKey.W)
+                {
+                    LegoGuy1.MoveForward();
+                }
+                if (cki.Key == ConsoleKey.S)
+                {
+                    LegoGuy1.MoveBackward();
+                }
+                if (cki.Key == ConsoleKey.A)
+                {
+                    LegoGuy1.MoveLeft();
+                }
+                if (cki.Key == ConsoleKey.D)
+                {
+                    LegoGuy1.MoveRight();
+                }
+                if (cki.Key == ConsoleKey.Q)
+                {
+                    LegoGuy1.Attack();
+                }
+                if (cki.Key == ConsoleKey.Spacebar)
+                {
+                    LegoGuy1.Jump();
+                }
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    LegoGuy1.Unlocks();
+                }
+        } while (cki.Key != ConsoleKey.Escape);
+
         }
     }
 }
